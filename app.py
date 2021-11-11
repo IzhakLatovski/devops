59ec0acf6674ba0aeb2ce34e0bee1058668255e6
Izhak Latovski <izhaklatovski@gmail.com>
2021-11-11 10:34:02 +0200

    working sample

--- app.py
+++ app.py
@@ -3,15 +3,24 @@ import db
 
 app = Flask(__name__)
 
+
+
 @app.route('/')
 def flask_mongodb_atlas():
-    return "flask mongodb atlas!"
+    skills = db.db.collection.find()
+    for skill in skills:
+          print(skill)
+
+    return "GOOOOOOOOOOOOOOOD"
 
 #test to insert data to the data base
 @app.route("/test")
 def test():
     db.db.collection.insert_one({"name": "John"})
+
     return "Connected to the data base!"
+
+
 
 if __name__ == '__main__':
     app.run(port=8000)
