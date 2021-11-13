cc56cd4c56f91f5a222501db5320f28320cc2ef4
Izhak Latovski <izhaklatovski@gmail.com>
2021-11-13 20:30:01 +0200

    adding new skill added

--- app.py
+++ app.py
@@ -4,39 +4,34 @@ import db
 app = Flask(__name__)
 
 
-
-# @app.route('/')
-# def flask_mongodb_atlas():
-#     skills = db.db.collection.find()
-#     for skill in skills:
-#           print(skill)
-
-#     return render_template("index.html")
-
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    print(request.method)
-    if request.method == 'POST':
-        if request.form.get('Encrypt') == 'Encrypt':
-            db.db.collection.insert_one({"name": "Izhak"})
-            print("Added new skill to database")
-            return render_template("index.html")
-        elif  request.form.get('Decrypt') == 'Decrypt':
-            # pass # do something else
-            print("Decrypted")
-            return render_template("index.html")
-        else:
-            # pass # unknown
-            return render_template("index.html")
-    elif request.method == 'GET':
-        return render_template("index.html")
-
-
-@app.route("/skills")
-def test():
     all_skills = db.db.collection.find({})
 
-    return render_template("index.html", skills=all_skills)
+    if request.method == 'POST':
+        if request.form.get('Encrypt') == 'Encrypt':
+            # db.db.collection.insert_one({"name": "Bash", "category": "Scripting", "date": "8/21", "description": "Basic scripting on Linux, using Bash", "icon": "https://e7.pngegg.com/pngimages/330/276/png-clipart-bash-shell-script-bourne-shell-scripting-language-unix-shell-shell-rectangle-logo.png"})
+            # db.db.collection.insert_one({"name": "Flask", "category": "Development", "date": "9/21", "description": "Python programming language, Flask specificly", "icon": "https://miro.medium.com/max/800/1*Q5EUk28Xc3iCDoMSkrd1_w.png"})
+            return render_template("index.html", skills=all_skills)
+        elif  request.form.get('Decrypt') == 'Decrypt':
+            return render_template("index.html", skills=all_skills)
+        elif request.form.get('Delete') == 'Delete':
+            db.db.collection.delete_one({"name": "Bash"})
+            return render_template("index.html", skills=all_skills)
+        else:
+            return render_template("index.html", skills=all_skills)
+    elif request.method == 'GET':
+        return render_template("index.html", skills=all_skills)
+
+@app.route("/newform", methods=['GET', 'POST'])
+def newform():
+    all_skills = db.db.collection.find({})
+
+    if request.method == 'GET':
+        return render_template("newform.html")
+    elif request.method == 'POST':
+        db.db.collection.insert_one({"name": request.form['name'], "category": request.form['category'], "date": request.form['date'], "description": request.form['description'], "icon": request.form['icon'], })
+        return render_template("index.html", skills=all_skills) 
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True, host='0.0.0.0')
