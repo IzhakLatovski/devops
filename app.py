b4d3b8b6221885d535ff813c4bdd0f4a2a5b6cd5
Izhak <your_email@example.com>
2021-11-15 13:59:40 +0200

    working mongo

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 from flask import Flask, render_template, request
 from werkzeug.utils import redirect
 import pymongo
-import db
+from pymongo import MongoClient
+# import db
 
 app = Flask(__name__)
 
+def get_db():
+    client = MongoClient(host='test_mongodb',
+                         port=27017, 
+                         username='root', 
+                         password='pass',
+                         authSource="admin")
+    db = client["skills"]
+    return db
+
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    all_skills = db.db.collection.find({})
+    db = get_db()
+
+    all_skills = db.skills.find({})
 
     if request.method == 'POST':
         if request.form.get('Add') == 'Add':
-            db.db.collection.insert_one({"name": request.form['name'], "category": request.form['category'], "date": request.form['date'], "description": request.form['description'], "icon": request.form['icon']})
-            return render_template("index.html", skills=all_skills) 
+                db.skills.insert_one({"name": request.form['name'], "category": request.form['category'], "date": request.form['date'], "description": request.form['description'], "icon": request.form['icon']})
+                return render_template("index.html", skills=all_skills) 
         else:
             return render_template("index.html", skills=all_skills)
     elif request.method == 'GET':
@@ -28,23 +40,18 @@ def editform():
     if request.method == 'GET':
         return render_template("editform.html")
 
-@app.route('/<path:path>', methods=['GET', 'POST', 'PUT'])
+@app.route('/<path:path>', methods=['GET', 'POST'])
 def skill(path):
-    found_skill = db.db.collection.find_one({"name": path})
+    db = get_db()
+
+    found_skill = db.skills.find_one({"name": path})
 
     if request.method == 'GET':
             return render_template("skill.html", skill=found_skill)
     if request.method == 'POST':
         if request.form.get('Delete') == 'Delete':
-            db.db.collection.delete_one({"name": path})
+            db.skills.delete_one({"name": path})
             return redirect("/")
-
-
-
-    # elif request.method == 'PUT':
-    #     if request.form.get('Edit') == 'Edit':
-    #         db.db.collection.update_one({"name": request.form['name'], "category": request.form['category'], "date": request.form['date'], "description": request.form['description'], "icon": request.form['icon'], })
-    #         return redirect("/")
 
 
 if __name__ == '__main__':
